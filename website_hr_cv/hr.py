294096726437a30ea087137f83ae454b6adc809f
HaojunZou <apollo_zhj@msn.com>
2015-12-02 17:08:26 +0100

    reformated hr_cv with original categ_id as tags

--- website_hr_cv/hr.py
+++ website_hr_cv/hr.py
@@ -28,15 +28,19 @@ _logger = logging.getLogger(__name__)
 class hr_employee(models.Model):
     _inherit = 'hr.employee'
 
+    @api.one
     def _assignment_tags(self):
-        self.all_categ_ids = (6,_,sorted(set([c.name for c in a.categ_ids for a in self.assignment_ids]+
-                                             [c.name for c in self.categ_ids])))
+        c0 = [item for sublist in self.assignment_ids.mapped(lambda r: r.categ_ids.mapped('id')) for item in sublist]
+        c1 = [c for c in self.skill_ids.mapped('id')]
+        self.all_categ_ids = [(6,_,set(c0+c1))]
 
     education = fields.Html(string='Educations', translate=True)
     assignment_ids = fields.One2many(comodel_name='hr.assignment', inverse_name='employee_id', string='Assignments')
-    categ_ids = fields.Many2many(comodel_name='crm.case.categ', string='Tags')
-    all_categ_ids = fields.Many2many(compute='_assignment_tags', string='All Tags')
-    anonymous = fields.Boolean('Anonymous',help="Your CV can be published, but without your name and picture. Use this only if it is sensitive, given your current engagement")
+    skill_ids = fields.One2many(comodel_name='hr.skill', inverse_name='employee_id', string='Skills')
+    all_categ_ids = fields.Many2many(comodel_name='crm.case.categ', compute='_assignment_tags', string='All Tags')
+    anonymous = fields.Boolean('Anonymous',help='Your CV can be published, but without your name and picture. Use this only if it is sensitive, given your current engagement')
+    location_ids = fields.Many2many(comodel_name='hr.location', string='Places')
+    language_ids = fields.Many2many(comodel_name='hr.language', string='Languages')
 
 class hr_assignment(models.Model):
     _name = 'hr.assignment'
@@ -48,29 +52,30 @@ class hr_assignment(models.Model):
     description = fields.Html(string='Description')
     sequence = fields.Integer(string='Sequence')
     published = fields.Boolean(string='Published')
-    location_ids = fields.Many2many(comodel_name='hr.location', string='Places')
-    date_from = fields.Date()
-    date_to   = fields.Date()
-    language_ids = fields.Many2many(comodel_name='hr.language')
-    
+    date_from = fields.Date(string='Date from')
+    date_to   = fields.Date(string='Date to')
+
 class hr_skill(models.Model):
-    _name="hr.skill"
+    _name='hr.skill'
+
     categ_id = fields.Many2one(comodel_name='crm.case.categ')
     employee_id = fields.Many2one(comodel_name='hr.employee', string='Employee')
     level = fields.Selection([('1','1 - 3 year'),('2','3 - 5 year'),('3','5 - more years')], string='Level', required=False)
-    
+
 class hr_location(models.Model):
-    _name="hr.location"
-    name = fields.Char(string='name', translate=True)
+    _name='hr.location'
+
+    name = fields.Char(string='Name', translate=True)
 
 class hr_language(models.Model):
-    _name="hr.language"
-    name = fields.Char(string='name', translate=True)
+    _name='hr.language'
+
+    name = fields.Char(string='Name', translate=True)
 
 
 class website_hr(http.Controller):
 
-    @http.route(['/consultants'], type='http', auth="public", website=True)
+    @http.route(['/consultants'], type='http', auth='public', website=True)
     def consultants(self, **post):
         return request.website.render("website_hr_cv.consultants_view",
             {'employee_ids': request.env['hr.employee'].sudo().search([],order="name")})
